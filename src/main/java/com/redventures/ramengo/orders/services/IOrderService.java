8bcf0a63783e05e8628f7595b7e87127a510c6f2
Debora <silvadeborafisio@gmail.com>
2024-06-02 01:32:33 -0300

    fix: remove method return for order calculation

--- src/main/java/com/redventures/ramengo/orders/services/IOrderService.java
+++ src/main/java/com/redventures/ramengo/orders/services/IOrderService.java
@@ -4,13 +4,11 @@ import com.redventures.ramengo.orders.domain.Order;
 import com.redventures.ramengo.orders.domain.dto.OrderRequest;
 import com.redventures.ramengo.orders.domain.dto.OrderView;
 
-import java.math.BigDecimal;
-
 public interface IOrderService {
 
     OrderView performOrder(OrderRequest order);
 
-    BigDecimal calculateTotalValue(Order order);
+    void calculateTotalValue(Order order);
 
     void populateOrderDescription(Order order);
 }
