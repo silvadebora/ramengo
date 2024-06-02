7583482000e731ad29a8dd356e690ccd218fb48c
Debora <silvadeborafisio@gmail.com>
2024-06-02 01:28:24 -0300

    feat: add service call to return order id

---
+++ src/main/java/com/redventures/ramengo/orders/services/IGenerateOrderIdService.java
@@ -0,0 +1,7 @@
+package com.redventures.ramengo.orders.services;
+
+public interface IGenerateOrderIdService {
+
+    String generateOrderId();
+
+}
---
+++ src/main/java/com/redventures/ramengo/orders/services/impl/GenerateOrderIdService.java
@@ -0,0 +1,38 @@
+package com.redventures.ramengo.orders.services.impl;
+
+import com.redventures.ramengo.orders.services.IGenerateOrderIdService;
+import com.redventures.ramengo.orders.util.OrderIdResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class GenerateOrderIdService implements IGenerateOrderIdService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${generate.api.url}")
+    private String apiUrl;
+
+    @Value("${api.key}")
+    private String apiKey;
+
+
+    @Override
+    public String generateOrderId() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("x-api-key", apiKey);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<OrderIdResponse> response =
+                restTemplate.exchange(apiUrl, HttpMethod.POST,
+                        entity, OrderIdResponse.class);
+
+        return response.getBody().getOrderId();
+    }
+}
