3ce20ba67dc47aeb76ac0ec84984a14b59c83cee
Debora <silvadeborafisio@gmail.com>
2024-06-04 09:59:20 -0300

    fix: fix problem with simpleMessageListenerContainer

--- src/main/java/com/redventures/ramengo/orders/config/AwsSqsConfiguration.java
+++ src/main/java/com/redventures/ramengo/orders/config/AwsSqsConfiguration.java
@@ -2,13 +2,24 @@ package com.redventures.ramengo.orders.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.config.QueueMessageHandlerFactory;
+import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
 import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.cloud.aws.messaging.listener.QueueMessageHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.handler.annotation.support.PayloadArgumentResolver;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
 
+import java.util.ArrayList;
+import java.util.List;
+/*https://stackoverflow.com/questions/43674465/error-while-connecting-to-aws-sqs-from-spring-boot*/
+/*https://stackoverflow.com/questions/74825473/spring-boot-3-0-0-sqs-java-lang-classnotfoundexception-org-springframework-me*/
 
 @EnableSqs
 @Configuration
@@ -22,13 +33,37 @@ public class AwsSqsConfiguration {
     private String secretKey;
 
     @Bean
-    public AmazonSQS amazonSQSBuilder(){
+    public AmazonSQSAsync amazonSQSAsync(){
         BasicAWSCredentials credentials = new BasicAWSCredentials(accessKeyId, secretKey);
-        return AmazonSQSClientBuilder
+        return AmazonSQSAsyncClientBuilder
                 .standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(region)
                 .build();
+    }
+
+    @Bean
+    public QueueMessagingTemplate queueMessagingTemplate() {
+        return new QueueMessagingTemplate(amazonSQSAsync());
+    }
+
+    @Bean
+    public SimpleMessageListenerContainerFactory simpleMessageListenerContainerFactory() {
+        SimpleMessageListenerContainerFactory msgListenerContainerFactory = new SimpleMessageListenerContainerFactory();
+        msgListenerContainerFactory.setAmazonSqs(amazonSQSAsync());
+        return msgListenerContainerFactory;
+    }
+
+    @Bean
+    public QueueMessageHandler queueMessageHandler() {
+        QueueMessageHandlerFactory factory = new QueueMessageHandlerFactory();
+        factory.setAmazonSqs(amazonSQSAsync());
+        QueueMessageHandler queueMessageHandler = factory.createQueueMessageHandler();
+        List<HandlerMethodArgumentResolver> list = new ArrayList<>();
+        HandlerMethodArgumentResolver resolver = new PayloadArgumentResolver(new MappingJackson2MessageConverter());
+        list.add(resolver);
+        queueMessageHandler.setArgumentResolvers(list);
+        return queueMessageHandler;
     }
 
 }
