a161a72d3394d0dbccb187262abbed71fe956a86
Debora <silvadeborafisio@gmail.com>
2024-06-04 08:42:45 -0300

    refactor: refactor security configuration name

--- src/main/java/com/redventures/ramengo/orders/config/SecurityConfiguration.java
+++ src/main/java/com/redventures/ramengo/orders/config/SecurityConfiguration.java
@@ -7,13 +7,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfiguration {
 
     @Autowired
     private ApiKeyAuthFilter filter;
